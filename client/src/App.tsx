1c212ef29e54fb75299585c174ad1a9868404d20
Valerie Racine <116108359+v-racine@users.noreply.github.com>
2026-02-18 19:55:17 -0800

    feat:add error handling views for user (#3)
    * feat:add error handling views for user
    
    * fix: typo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,34 +7,40 @@ function App() {
   const [toc, setToc] = useState<TOCType | null>(null);
   const [selectedChapter, setSelectedChapter] = useState<string | null>(null);
   const [chapterContent, setChapterContent] = useState<string | null>(null);
+  const [tocError, setTocError] = useState<string | null>(null);
+  const [chapterError, setChapterError] = useState<string | null>(null);
 
   useEffect(() => {
     fetch("/api/toc")
       .then((res) => {
         if (!res.ok) {
-          throw new Error(`HTTP error! status: ${res.status}`);
+          throw new Error(
+            `Failed to load table of contents. Status: ${res.status}`,
+          );
         }
         return res.json();
       })
       .then((data) => setToc(data))
-      .catch((error) => console.error("Error fetching TOC:", error));
+      .catch((error) => setTocError(error.message));
   }, []);
 
   useEffect(() => {
     if (!selectedChapter) return;
+    setChapterError(null);
+
     const urlParam = selectedChapter.toLowerCase().replaceAll(" ", "-");
     fetch(`/api/${urlParam}`)
       .then((res) => {
         if (!res.ok) {
-          throw new Error(`HTTP error! status: ${res.status}`);
+          throw new Error(`Failed to load chapter. Status: ${res.status}`);
         }
         return res.text();
       })
       .then((data) => setChapterContent(data))
-      .catch((error) =>
-        console.error(`Error fetching chapter ${selectedChapter}:`, error),
-      );
+      .catch((error) => setChapterError(error.message));
   }, [selectedChapter]);
+
+  if (tocError) return <p>Error: {tocError}</p>;
 
   return (
     <div id="app">
@@ -43,6 +49,7 @@ function App() {
         <ChapterView
           chapter={selectedChapter}
           content={chapterContent}
+          error={chapterError}
           onBack={() => {
             setSelectedChapter(null);
             setChapterContent(null);
